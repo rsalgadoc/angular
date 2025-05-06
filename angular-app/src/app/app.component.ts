859043c7f9a31c317f829cd3208aff3736320e72
rsalgadoc <rodrigo.salgado.cordova@gmail.com>
2025-05-05 23:58:55 -0400

    add example of use nfIf and ngfor

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hola Mundo Angular';
+  title = 'Hola Mundo Angular!';
+  users = ['uno', 'dos', 'tres', 'cuatro'];
+
+  visible: boolean = false; 
+
+  setVisible(): void{
+    this.visible = this.visible? false : true;
+    console.log('click en boton');
+  }
 }
