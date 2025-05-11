f79571412325a6e8d2d0575d6b0708fff043246c
rsalgadoc <rodrigo.salgado.cordova@gmail.com>
2025-05-11 15:29:51 -0400

    add new data for users

--- user-backed/src/main/resources/data.sql
+++ user-backed/src/main/resources/data.sql
@@ -1,6 +1,8 @@
-INSERT INTO users(name,lastname,email,username,password) VALUES('Juan', 'Sala', 'juan@gmail.com','jsalsa','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS');
-INSERT INTO users(name,lastname,email,username,password) VALUES('Pedro', 'Tomate', 'pedro@gmail.com','ptomate','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS');
-INSERT INTO users(name,lastname,email,username,password) VALUES('Diego', 'Pan', 'diego@gmail.com','dpan','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS');
+INSERT INTO users(name,lastname,email,username,password) VALUES('Rodrigo', 'Salgado', 'juan@gmail.com','rsalgado','$2a$10$J9x0CuS9f86J2TyERb3rYOUy/D.wUXksYPECyHhDZPAr.maj7/U8O');
+INSERT INTO users(name,lastname,email,username,password) VALUES('Pedro', 'Perez', 'pedro@gmail.com','pperez','$2a$10$J9x0CuS9f86J2TyERb3rYOUy/D.wUXksYPECyHhDZPAr.maj7/U8O');
+INSERT INTO users(name,lastname,email,username,password) VALUES('Fernando ', 'Gonzalez', 'fgonzalez@gmail.com','fgonzalez','$2a$10$J9x0CuS9f86J2TyERb3rYOUy/D.wUXksYPECyHhDZPAr.maj7/U8O');
+INSERT INTO users(name,lastname,email,username,password) VALUES('Nicolas ', 'Prieto', 'nprieto@gmail.com','nprieto','$2a$10$J9x0CuS9f86J2TyERb3rYOUy/D.wUXksYPECyHhDZPAr.maj7/U8O');
+INSERT INTO users(name,lastname,email,username,password) VALUES('Alejandro ', 'Rios', 'arios@gmail.com','arios','$2a$10$J9x0CuS9f86J2TyERb3rYOUy/D.wUXksYPECyHhDZPAr.maj7/U8O');
 
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 INSERT INTO roles(name) VALUES('ROLE_USER');
@@ -9,7 +11,7 @@ INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO users_roles(user_id,role_id) VALUES(1,1);
 INSERT INTO users_roles(user_id,role_id) VALUES(2,1);
 INSERT INTO users_roles(user_id,role_id) VALUES(3,2);
-
-
+INSERT INTO users_roles(user_id,role_id) VALUES(4,2);
+INSERT INTO users_roles(user_id,role_id) VALUES(5,2);
 
 
